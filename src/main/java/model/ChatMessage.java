10b9389486a4f91613dce135c938ce0d85cc2ffb
tray0019 <tray0019@algonquinlive.com>
2025-08-12 12:30:33 -0400

    created a sendMessage app with @MessageMapping and @SendTo annotation. The @MessageMapping runs the method to send to a WebSocket when client sends a message. The @SendTo sends it all to the subscriber of the chat. Also provided GetMapping chat method for chat interface.

---
+++ src/main/java/model/ChatMessage.java
@@ -0,0 +1,14 @@
+package model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ChatMessage {
+
+    private Long id;
+    private String sender;
+    private String content;
+
+}
